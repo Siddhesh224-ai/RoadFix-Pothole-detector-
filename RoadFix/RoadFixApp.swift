6c001f67522934d514122a9bf9d294bdf2d0dce4
Siddhesh Mutha <Siddhesh Mutha>
2024-12-02 17:21:11 +0530

    Initial Commit

---
+++ RoadFix/RoadFixApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoadFixApp.swift
+//  RoadFix
+//
+//  Created by Siddhesh Mutha on 02/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RoadFixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
